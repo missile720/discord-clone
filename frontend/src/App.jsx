2f35927244312682e381abe4cc86cd31605f2637
Misael Bolainez <misaelbolainez@gmail.com>
2023-10-04 15:02:52 -0700

    feat(background): Added the initial background to the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Main from './components/Main'
 
 function App() {
 
   return (
     <div>
-      Hello World
+      <Main/>
     </div>
   )
 }
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login-container">
+      <div className="login-left">Welcome back!</div>
+      <div className="login-right">QR code</div>
+    </div>
+  );
+}
+
+export default Login;
